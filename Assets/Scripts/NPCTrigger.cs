5b17c38f44a34aa30d828519a2894775c878a952
MeZeOfficial <lucaschauveau007@gmail.com>
2024-02-24 16:56:18 -0500

    input box working, grabbing text not yet

--- Assets/Scripts/NPCTrigger.cs
+++ Assets/Scripts/NPCTrigger.cs
@@ -1,28 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
 public class NPCTrigger : MonoBehaviour
 {
     public BoxCollider2D NPC_Collider;
-    public TextMeshProUGUI interactText;
+    public GameObject interactText;
+    public GameObject inputPrompt;
+    public TMP_Text playerText;
 
     void Awake()
     {
-        interactText.enabled = false;
+        interactText.SetActive(false);
+        InputClose();
+        playerText = GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        if(interactText.activeInHierarchy == true && Input.GetKey(KeyCode.E))
+        {
+            inputPrompt.SetActive(true);
+            Debug.Log(playerText.text);
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Entered Collider");
-        interactText.enabled = true;
+        interactText.SetActive(true);
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         Debug.Log("Exited Collider");
-        interactText.enabled = false;
+        interactText.SetActive(false);
+    }
+
+    public void InputClose()
+    {
+        inputPrompt.SetActive(false);
     }
 }

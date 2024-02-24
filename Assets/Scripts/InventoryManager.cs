d8ebe081b1575a3fe4de9d4679876fe1ae388eb6
lufainfire <lufainfire@gmail.com>
2024-02-24 17:28:18 -0500

    Update InventoryManager.cs

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    public static InventoryManager instance;
+
     public InventorySlot[] slots;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Update is called once per frame
     void Update()
     {

563be4c2770275eed008a32a316b25b7f5de07ee
lufainfire <lufainfire@gmail.com>
2024-02-24 19:51:44 -0500

    update

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image slotImage;
+
+    public OnItemPickup(Item item)
     {
-        
+        slotImage = item.image;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image imgae;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

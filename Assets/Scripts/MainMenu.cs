bf62a05334852b0d2ffcf36b01dce292c78953b1
Andrei <ierdna100@yahoo.com>
2024-02-25 13:39:45 -0500

    Final Push

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(scenename);
+        SceneManager.LoadScene(scenename, LoadSceneMode.Single);
     }
 
     public void OnQuit()
